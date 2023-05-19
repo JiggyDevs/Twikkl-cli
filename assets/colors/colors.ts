3fc9f24d2a0d1cbbf51826a20dc2d40e54d358e8
Vcky4 <okon.victor.u@gmail.com>
2023-05-19 16:43:46 +0100

    add assests

---
+++ assets/colors/colors.ts
@@ -0,0 +1,9 @@
+const colors = {
+  white: '#FFFFFF',
+  black: '#000000',
+  background:'#F8F8F8',
+  grayInactive:"#9D9D9D",
+  buttonInactive:'#BEBEBE'
+};
+
+export default colors;
---
+++ assets/endpoints/endpoints.ts
@@ -0,0 +1,7 @@
+const endpoints = {
+    baseUrl: 'https://api',
+    login: '/auth/login',
+    register: '/auth/sign-up',
+}
+
+export default endpoints;
