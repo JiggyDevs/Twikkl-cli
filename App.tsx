94946f0d55f2d443114c4ac04e9ab51050f938df
Vcky4 <okon.victor.u@gmail.com>
2023-05-19 16:40:40 +0100

    add some config files

--- App.tsx
+++ App.tsx
@@ -1,118 +1,44 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useEffect, useContext, useState } from "react";
+import { View, Text, StatusBar, Platform } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import Toast from 'react-native-toast-message';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Splash from "./src/screens/onboarding/Splash";
+import { AuthContext, AuthContextProvider } from "./context/AuthContext";
+import AuthStack from "./src/navigation/stacks/AuthStack";
+import MainStack from "./src/navigation/stacks/MainStack";
+import OnboardingScene from "./src/screens/onboarding/OnboardingScene";
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const RootNavigator: React.FC = () => {
+  const authContext = useContext(AuthContext);
+  if (!authContext) {
+    return null
+  }
+  const { token } = authContext
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => { setTimeout(() => setLoading(false), 2000) });
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <NavigationContainer>
+      {
+        isLoading ? <Splash /> :
+            token ? <MainStack /> :
+              <AuthStack />}
+    </NavigationContainer>
+  )
+}
+export default function App() {
+  if (Platform.OS == 'ios') {
+    StatusBar.setBarStyle('light-content', true);	//<<--- add this
+  }
+  return (
+    <>
+      <AuthContextProvider>
+        <StatusBar backgroundColor={"#000"} />
+        <RootNavigator />
+      </AuthContextProvider>
+      <Toast />
+    </>
   );
 }
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
